fc100d47a444eaf975b54409f89c162ddd14c438
phamkhoa1303 <pham.duykhoa1303@gmail.com>
2022-03-06 16:07:12 +0700

    init

---
+++ public/script/index.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+  setTimeout(function () {
+    document.getElementById("universe").classList.add("opendis");
+  }, 1000);
+  setTimeout(function () {
+    alert("Press F11 key to make it more vivid!");
+    init();
+  }, 2000);
+};
+
+function init() {
+    document.getElementById("clickMe").onclick = function (e) {
+        this.classList.add("closedis");
+        document.getElementById("universe").classList.add("closedis");
+        document.getElementById("firework").classList.add("opendis");
+        document.getElementById("firework").classList.add("backgroundFireword")
+        setTimeout(function () {
+            RunFireWork();
+        },450)
+    }
+}
